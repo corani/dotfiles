fa1ba89f888224cad20a01a3958b18049acf8545
Daniel Bos <corani@gmail.com>
2020-02-29 18:35:52 +0800

    add i3

---
+++ .config/i3/i3lock.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+case "blur" in
+    blur)
+        strength=5
+        effect="boxblur=$strength:$strength"
+        ;;
+    pixelate)
+        scale=16
+        effect="scale=iw/$scale:ih/$scale,scale=$scale*iw:$scale*ih:flags=neighbor"
+        ;;
+esac
+
+ffmpeg -loglevel quiet -y -f x11grab -video_size 2560x1440 -i $DISPLAY -i ~/.config/i3/lock2.png -filter_complex "$effect,overlay=(main_w-overlay_w)/2+14:(main_h-overlay_h)/2+14" -vframes 1 /tmp/lock_screen.png
+i3lock -i /tmp/lock_screen.png
